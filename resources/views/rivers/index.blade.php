6e05507d7b5e0b361553772b4b211806e5444d2f
Vanessa Baylon <vrabaylon@gmail.com>
2020-02-13 15:36:58 +0800

    FE-002: edited Device Locations page

--- resources/views/rivers/index.blade.php
+++ resources/views/rivers/index.blade.php
@@ -3,20 +3,20 @@
 @section('main')
 <div class="row">
 <div class="col-sm-12">
-    <h1 class="display-3">Rivers</h1> 
+    <h1 class="page-title">Device Locations</h1> 
     @if(auth()->user()->is_admin==1)
     <div>
-    <a style="margin: 19px;" href="{{ route('rivers.create')}}" class="btn btn-primary">New River</a>
+    <a style="margin: 19px;" href="{{ route('rivers.create')}}" class="btn btn-primary ml-0">Add New Location</a>
     </div>     
     @else
     @endif
   <table class="table table-striped">
-    <thead>
+    <thead class="font-bold">
         <tr>
           <td>River ID</td>
           <td>Location</td>
           <td>River Name</td>
-          <td colspan = 2>Actions</td>
+          <td>Actions</td>
         </tr>
     </thead>
     <tbody>
