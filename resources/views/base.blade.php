f85cbbda1aa44fbe7c2d15376346473683f7c5d9
Vanessa Baylon <vrabaylon@gmail.com>
2020-02-13 14:42:49 +0800

    FE-001: added css file for easy UI editing

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -4,6 +4,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>River Monitoring System</title>
   <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
+  <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css" />
   <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
   <link href="https://cdn.jsdelivr.net/npm/vanilla-datatables@latest/dist/vanilla-dataTables.min.css" rel="stylesheet" type="text/css">
   <script src="https://cdn.jsdelivr.net/npm/vanilla-datatables@latest/dist/vanilla-dataTables.min.js" type="text/javascript"></script>
