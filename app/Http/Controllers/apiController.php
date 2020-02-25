1677c4b7123ae7595223f4512b1cc5bcf35448e5
Jari Sebastian Mesina <jarimesina1234@example.com>
2020-02-25 18:41:17 +0800

    Optimized code

--- app/Http/Controllers/apiController.php
+++ app/Http/Controllers/apiController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
 use App\Dates;
 use App\River;
+use App\Days;
 
 class apiController extends Controller
 {
@@ -16,11 +17,50 @@ class apiController extends Controller
      */
     public function index(Request $request)
     {
+        $i = 0;
+        $river= River::find($request->id);
+        Dates::create(['start' => $request->start,'end' =>$request->end]);
+        $client = new Client();
+        $date = Dates::latest()->first();
+        $start = explode('-', $date->start);
+        $end = explode('-', $date->end);
+        //get all days in db
+        $days = Days::all();
+        //check if start date or end date are in database
+        while($i < count($days)){
+            if(strcmp($days[$i]->date,$request->start)){
+                dd("YES");
+            }
+            else{
+                dd("NO");
+            }
+            if(strcmp($days[$i]->date,$request->start)){
+                
+            }  
+            $i++;
+        }
 
-        //Dates::create(['start' =>$request->start,'end' =>$request->end]);
+        $url = 'https://api.thingspeak.com/channels/'.$river->channel.'/fields/2.json?api_key='.$river->key.'&start='.$start[0].'-'.$start[1].'-'.$start[2].'&end='.$end[0].'-'.$end[1].'-'.$end[2];
 
-      
-        return redirect()->back();
+        $res2 = $client->request('GET',$url);
+        $temp = json_decode($res2->getBody()->getContents()); //--original
+        $temp = $temp->feeds;
+
+        $cart = array();
+        $id = array();
+
+        foreach($temp as $element){
+            array_push($cart, (float)$element->field2);
+            array_push($id, $element->entry_id);
+        }
+
+        $cart2 = new Collection();
+        $id2 = new Collection();
+        $data = collect($cart);
+        $labels = collect($id);
+ 
+        // return response()->json(compact('labels', 'data'));
+          
     }
 
     /**
@@ -31,26 +71,14 @@ class apiController extends Controller
     public function process(Request $request)
     {
         $river= River::find($request->id);
-        // $key = River:: $request->id;
         Dates::create(['start' => $request->start,'end' =>$request->end]);
-
         $client = new Client();
         $date = Dates::latest()->first();
         $start = explode('-', $date->start);
         $end = explode('-', $date->end);
-        // dump($start);
-        // dump($end);
-        // dd($date);
-        // dump($start[0]);
-        // dump($start[1]);
-        // dump($start[2]);
-        // dump($end[0]);
-        // dump($end[1]);
-        // dump($end[2]);
+
         $url = 'https://api.thingspeak.com/channels/'.$river->channel.'/fields/2.json?api_key='.$river->key.'&start='.$start[0].'-'.$start[1].'-'.$start[2].'&end='.$end[0].'-'.$end[1].'-'.$end[2];
 
-        // $url = 'https://api.thingspeak.com/channels/' . $river->channel . '/fields/2.json?api_key=' . $river->key . '&start='.$start[0].'-'.$start[1].'-'.$start[2].'&end='.$end[0].'-'.$end[1].'-'.$end[2];
-        // dump($url);
         $res2 = $client->request('GET',$url);
         $temp = json_decode($res2->getBody()->getContents()); //--original
         $temp = $temp->feeds;
