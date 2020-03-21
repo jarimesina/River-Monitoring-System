5286cf4e4148bb8e6b56eb59f0a1578b9d6f49b6
Jari Sebastian Mesina <jarimesina1234@example.com>
2020-03-21 12:55:03 +0800

    Added export features

--- resources/views/rivers/riverDetails.blade.php
+++ resources/views/rivers/riverDetails.blade.php
@@ -277,13 +277,18 @@
     </table>
   </div>
 </div>
-
+ 
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/jszip-2.5.0/dt-1.10.20/b-1.6.1/b-html5-1.6.1/b-print-1.6.1/datatables.min.css"/>
+ 
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/v/dt/jszip-2.5.0/dt-1.10.20/b-1.6.1/b-html5-1.6.1/b-print-1.6.1/datatables.min.js"></script>
 <script>
   $(document).ready(function(){
   $('.input-daterange').datepicker({
     todayBtn:'linked',
     format:'yyyy-mm-dd',
-    autoclose:true
+    autoclose:true,
   });
 
   load_data();
@@ -291,6 +296,10 @@
   function load_data(from_date = '', to_date = '')
   {
     $('#order_table').DataTable({
+      dom: 'Bfrtip',
+      buttons: [
+          'copy', 'csv', 'excel', 'pdf', 'print'
+      ],
       processing: true,
       serverSide: true,
       ajax: {
@@ -313,7 +322,7 @@
         {
         data:'temperature',
         name:'temperature'
-        },
+        }
       ]
     });
   }
